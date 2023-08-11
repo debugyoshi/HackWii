4d6c72508969375689cefc330c1205b7222d7b48
debugyoshi <93015978+debugyoshi@users.noreply.github.com>
2023-08-11 12:24:16 -0400

    Updated Homebrew.sh (spell checked + files referenced)

--- scripts/Homebrew.sh
+++ scripts/Homebrew.sh
@@ -1,7 +1,7 @@
 clear
-echo "You should now see a screen that warns you about scams. You don't need to worry about this. Just wait, and then press the 1 button. You will then see a screen that details what can be installed. If it says that BootMii can only be installed as an IOS, that's normal. Also, keep in mind that you can't install BootMii on a Wii Mini without hardware mods, as it requires an SD card.
+echo "You should now see a screen that warns you about scams. You don't need to worry about this. Just wait, and then press the 1 button. You will then see a screen that     details what can be installed. If it says that BootMii can only be installed as an IOS, that's normal. Also, keep in mind that you can't install BootMii on a Wii Mini  without hardware mods, as it requires an SD card.
 
-Select Continue. Then select 'Install Homebrew Channel'. Select Continue, wait, then select back, and select 'Install BootMii as IOS'. If you can't install BootMii at this moment, but want to install it later (or uninstall something), run HackWiiAgain.sh in the HackWii folder, then the scripts folder.
+Select Continue. Then select 'Install Homebrew Channel'. Select Continue, wait, then select back, and select 'Install BootMii as IOS'. If you can't install BootMii at  this moment, but want to install it later (or uninstall something), run HackWiiAgain.sh in the HackWii folder, then the scripts folder.
 
 Select Continue, wait, then select 'Install BootMii as boot2' if you can. If the option is not available, select Exit, and it should take you to the homebrew channel."
 
@@ -29,8 +29,18 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Make a NAND backup (Protects your Wii, requires BootMii)")
+		cd
+		cd HackWii
+		cd scripts
+		chmod +x NAND.sh
+		./NAND.sh
        ;;
 	   "Priiloader (Choose this if you couldn't install BootMii)")
+	   cd
+		cd HackWii
+		cd scripts
+		chmod +x Priiloader.sh
+		./Priiloader.sh
 		;;
 		"Quit (I can take it from here)")
 		clear
